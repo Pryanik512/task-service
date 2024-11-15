9b4470c9a4ceb66742cd523a783ac3e0da536880
dmar <dima69512@gmail.com>
2024-11-15 18:36:23 +0400

    [AOP] Added AspectJ dependencies and TaskAspect class

---
+++ src/main/java/com/arishev/task/aspect/TaskAspect.java
@@ -0,0 +1,10 @@
+package com.arishev.task.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TaskAspect {
+
+}
